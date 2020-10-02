348d32f0d15de97cc67e72633820045f958a185f
donRumata <donrumata03@gmail.com>
2020-10-02 22:30:55 +0300

    reorder GA params

--- GA/GA.h
+++ GA/GA.h
@@ -17,12 +17,6 @@ struct GA_params
 	size_t epoch_num = 0;
 
 
-	double mutation_percent_sigma = 0;
-	double target_gene_mutation_number = 0;
-	bool cut_mutations = false;
-
-	bool allow_multithreading = false;
-	size_t threads = std::thread::hardware_concurrency() - 2;
 
 	/// DELETED: double best_genome_percent = 0.05;
 
@@ -30,12 +24,19 @@ struct GA_params
 	double elite_fit_pow = 2;
 	double parent_fit_pow = 0.3;
 
-	GA::crossover_mode crossover_mode = GA::crossover_mode::low_variance_genetic;
+	double hazing_percent = 0.4;
 
-	double hazing_percent = 0.3;
+	double mutation_percent_sigma = 0;
+	double target_gene_mutation_number = 0;
+	bool cut_mutations = true;
+
+	GA::crossover_mode crossover_mode = GA::crossover_mode::low_variance_genetic;
 	std::optional<double> exiting_fitness_value = {};
 
 	GA::GA_operation_set custom_operations = {};
+
+	bool allow_multithreading = false;
+	size_t threads = std::thread::hardware_concurrency() - 2;
 
 	void set_default_epoch_num(size_t total_computations) {
 		/// TODO: find the optimal fraction by testing:
