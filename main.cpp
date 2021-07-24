6878d301b803530b3581c4d0ffea209cc94a9398
donRumata03 <donrumata03@gmail.com>
2021-07-24 22:53:03 +0300

    Decrease pow in computing upper mean to make  it work in linux

--- main.cpp
+++ main.cpp
@@ -43,7 +43,9 @@ int main()
 	/// 									GA computation distribution
 //	 std::cout << n_atic_mean({1, 1, 1, 23, 43, 64, 50, 100, 99, 150}, 0.5) << std::endl;
 	// run_GA_with_iteration_distribution();
-	// explore_performance_vs_epoch_pow();
+	explore_performance_vs_epoch_pow();
+
+	std::cout << sizeof(double) << " " << sizeof(char) << std::endl;
 
 	/// 										Annealing:
 	// annealing_permutational_mutation_test();
@@ -98,7 +100,7 @@ int main()
 
 	// GA_adapter_test();
 
-	std::cout << distribute_computations_defaultly(1000000) << std::endl;
+	// std::cout << distribute_computations_defaultly(1000000) << std::endl;
 
 	return 0;
 }
